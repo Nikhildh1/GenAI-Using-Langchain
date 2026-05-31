7c78918b741f392ed62ba394a9d72e8093246e1b
Nikhildh1 <nikhildhawan356@gmail.com>
2026-05-31 13:45:51 +0530

    Initial Commit

---
+++ 2. ChatModels/1. ChatModel_OpenAI.py
@@ -0,0 +1,15 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model=ChatOpenAI(model="gpt-4")
+
+ans=model.invoke("What is the capital of India?")
+
+# This will print many things like content (Which will contain actual ans for the question), 
+# additional kwargs, tokens, model_names and many other things.
+print(ans)
+
+# If we want only answer
+print(ans.content)
